2bb677da967adc42923093be75e615402326cf02
Massimo Bestazza <43986382+mbestazza@users.noreply.github.com>
2019-01-16 10:39:45 +0100

    update with SSL

--- trigger/mqtt/trigger.go
+++ trigger/mqtt/trigger.go
@@ -2,26 +2,35 @@ package mqtt
 
 import (
 	"context"
-	"encoding/json"
-	"strconv"
-	"time"
 
-	"github.com/TIBCOSoftware/flogo-lib/core/data"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/support"
+
+
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
 	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+
 	"github.com/TIBCOSoftware/flogo-lib/logger"
 	"github.com/eclipse/paho.mqtt.golang"
+	"time"
 )
 
 // log is the default package logger
-var log = logger.GetLogger("trigger-flogo-mqtt")
+var log = logger.GetLogger("trigger-tibco-mqtt")
 
 // MqttTrigger is simple MQTT trigger
 type MqttTrigger struct {
-	metadata       *trigger.Metadata
-	client         mqtt.Client
-	config         *trigger.Config
-	handlers       []*trigger.Handler
-	topicToHandler map[string]*trigger.Handler
+	metadata         *trigger.Metadata
+	runner           action.Runner
+	client           mqtt.Client
+	config           *trigger.Config
+	topicToActionURI map[string]string
 }
 
 //NewFactory create a new Trigger factory
@@ -44,13 +53,12 @@ func (t *MqttTrigger) Metadata() *trigger.Metadata {
 	return t.metadata
 }
 
-// Initialize implements trigger.Initializable.Initialize
-func (t *MqttTrigger) Initialize(ctx trigger.InitContext) error {
-	t.handlers = ctx.GetHandlers()
-	return nil
+// Init implements ext.Trigger.Init
+func (t *MqttTrigger) Init(runner action.Runner) {
+	t.runner = runner
 }
 
-// Start implements trigger.Trigger.Start
+// Start implements ext.Trigger.Start
 func (t *MqttTrigger) Start() error {
 
 	opts := mqtt.NewClientOptions()
@@ -58,7 +66,12 @@ func (t *MqttTrigger) Start() error {
 	opts.SetClientID(t.config.GetSetting("id"))
 	opts.SetUsername(t.config.GetSetting("user"))
 	opts.SetPassword(t.config.GetSetting("password"))
-	b, err := data.CoerceToBoolean(t.config.Settings["cleansess"])
+
+
+
+
+
+	b, err := strconv.ParseBool(t.config.GetSetting("cleansess"))
 	if err != nil {
 		log.Error("Error converting \"cleansess\" to a boolean ", err.Error())
 		return err
@@ -73,13 +86,18 @@ func (t *MqttTrigger) Start() error {
 		//TODO we should handle other types, since mqtt message format are data-agnostic
 		payload := string(msg.Payload())
 		log.Debug("Received msg:", payload)
-		handler, found := t.topicToHandler[topic]
+		actionURI, found := t.topicToActionURI[topic]
 		if found {
-			t.RunHandler(handler, payload)
+			t.RunAction(actionURI, payload)
 		} else {
-			log.Errorf("handler for topic '%s' not found", topic)
+			log.Errorf("Topic %s not found", t.topicToActionURI[topic])
 		}
 	})
+
+
+	//set tls config
+	tlsConfig := NewTLSConfig("")
+	opts.SetTLSConfig(tlsConfig)
 
 	client := mqtt.NewClient(opts)
 	t.client = client
@@ -87,24 +105,24 @@ func (t *MqttTrigger) Start() error {
 		panic(token.Error())
 	}
 
-	i, err := data.CoerceToDouble(t.config.Settings["qos"])
+	i, err := strconv.Atoi(t.config.GetSetting("qos"))
 	if err != nil {
 		log.Error("Error converting \"qos\" to an integer ", err.Error())
 		return err
 	}
 
-	t.topicToHandler = make(map[string]*trigger.Handler)
+	t.topicToActionURI = make(map[string]string)
 
-	for _, handler := range t.handlers {
+	for _, handlerCfg := range t.config.Handlers {
 
-		topic := handler.GetStringSetting("topic")
+		topic := handlerCfg.GetSetting("topic")
 
 		if token := t.client.Subscribe(topic, byte(i), nil); token.Wait() && token.Error() != nil {
 			log.Errorf("Error subscribing to topic %s: %s", topic, token.Error())
-			return token.Error()
+			panic(token.Error())
 		} else {
-			log.Debugf("Subscribed to topic: %s, will trigger handler: %s", topic, handler)
-			t.topicToHandler[topic] = handler
+			log.Debugf("Suscribed to topic: %s, will trigger actionId: %s", topic, handlerCfg.ActionId)
+			t.topicToActionURI[topic] = handlerCfg.ActionId
 		}
 	}
 
@@ -114,8 +132,8 @@ func (t *MqttTrigger) Start() error {
 // Stop implements ext.Trigger.Stop
 func (t *MqttTrigger) Stop() error {
 	//unsubscribe from topic
+	log.Debug("Unsubcribing from topic: ", t.config.Settings["topic"])
 	for _, handlerCfg := range t.config.Handlers {
-		log.Debug("Unsubscribing from topic: ", handlerCfg.GetSetting("topic"))
 		if token := t.client.Unsubscribe(handlerCfg.GetSetting("topic")); token.Wait() && token.Error() != nil {
 			log.Errorf("Error unsubscribing from topic %s: %s", handlerCfg.Settings["topic"], token.Error())
 		}
@@ -126,38 +144,34 @@ func (t *MqttTrigger) Stop() error {
 	return nil
 }
 
-// RunHandler runs the handler and associated action
-func (t *MqttTrigger) RunHandler(handler *trigger.Handler, payload string) {
+// RunAction starts a new Process Instance
+func (t *MqttTrigger) RunAction(actionURI string, payload string) {
 
-	trgData := make(map[string]interface{})
-	trgData["message"] = payload
+	req := t.constructStartRequest(payload)
+	//err := json.NewDecoder(strings.NewReader(payload)).Decode(req)
+	//if err != nil {
+	//	//http.Error(w, err.Error(), http.StatusBadRequest)
+	//	log.Error("Error Starting action ", err.Error())
+	//	return
+	//}
 
-	results, err := handler.Handle(context.Background(), trgData)
+	//todo handle error
+	startAttrs, _ := t.metadata.OutputsToAttrs(req.Data, false)
 
+	action := action.Get(actionURI)
+	context := trigger.NewContext(context.Background(), startAttrs)
+	_, replyData, err := t.runner.Run(context, action, actionURI, nil)
 	if err != nil {
 		log.Error("Error starting action: ", err.Error())
 	}
-
-	log.Debugf("Ran Handler: [%s]", handler)
-
-	var replyData interface{}
-
-	if len(results) != 0 {
-		dataAttr, ok := results["data"]
-		if ok {
-			replyData = dataAttr.Value()
-		}
-	}
+	log.Debugf("Ran action: [%s]", actionURI)
 
 	if replyData != nil {
-		dataJson, err := json.Marshal(replyData)
+		data, err := json.Marshal(replyData)
 		if err != nil {
 			log.Error(err)
 		} else {
-			replyTo := handler.GetStringSetting("topic")
-			if replyTo != "" {
-				t.publishMessage(replyTo, string(dataJson))
-			}
+			t.publishMessage(req.ReplyTo, string(data))
 		}
 	}
 }
@@ -177,10 +191,60 @@ func (t *MqttTrigger) publishMessage(topic string, message string) {
 		return
 	}
 	token := t.client.Publish(topic, byte(qos), false, message)
+
 	sent := token.WaitTimeout(5000 * time.Millisecond)
 	if !sent {
 		// Timeout occurred
 		log.Errorf("Timeout occurred while trying to publish to topic '%s'", topic)
 		return
 	}
+}
+
+func (t *MqttTrigger) constructStartRequest(message string) *StartRequest {
+	//TODO how to handle reply to, reply feature
+	req := &StartRequest{}
+	data := make(map[string]interface{})
+	data["message"] = message
+	req.Data = data
+	return req
+}
+
+// NewTLSConfig creates a TLS configuration for the specified 'thing'
+func NewTLSConfig(thingName string) *tls.Config {
+	// Import root CA
+	certpool := x509.NewCertPool()
+	pemCerts, err := ioutil.ReadFile("root-CA.pem")
+	if err == nil {
+		certpool.AppendCertsFromPEM(pemCerts)
+	}
+
+	//thingDir := "things/" + thingName + "/"
+
+	// Import client certificate/key pair for the specified 'thing'
+	cert, err := tls.LoadX509KeyPair("device.pem.crt", "device.pem.key")
+	if err != nil {
+		panic(err)
+	}
+
+	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+
+	return &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{cert},
+	}
+}
+
+// StartRequest describes a request for starting a ProcessInstance
+type StartRequest struct {
+	ProcessURI  string                 `json:"flowUri"`
+	Data        map[string]interface{} `json:"data"`
+	Interceptor *support.Interceptor   `json:"interceptor"`
+	Patch       *support.Patch         `json:"patch"`
+	ReplyTo     string                 `json:"replyTo"`
 }
